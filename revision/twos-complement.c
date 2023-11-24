286f88dfff6ef1a67bf9cc89aea9c1eff2ac0454
Aisha Nauman <aishanauman01@gmail.com>
2023-11-24 12:36:51 +1100

    twos complement

--- revision/twos-complement.c
+++ revision/twos-complement.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include <stdint.h>
+
+
+// write a program that takes in a signed 1 byte number
+// -15 = 15
+// 15 = -15
+
+int8_t twos_complement(uint8_t num) {
+    // subtract 1 from num
+    // flip all bits
+    return ~(num - 1);
+    // return -num;
+}
+
+int main(void) {
+    uint8_t num = 15; // 11111111
+    printf("the original number is: %d and positive version is this: %d\n", num, twos_complement(num));
+    return 0;
+}
